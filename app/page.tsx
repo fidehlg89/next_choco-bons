2d2ea86cc6cf2eeb94dc126928d409eb348c6039
Fidel Ernesto <fidehlg89@gmail.com>
2024-03-23 00:04:16 -0400

    remove unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import styles from '@/app/ui/home.module.css';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
-import Link from 'next/link';
 
 export default function Page() {
   return (
