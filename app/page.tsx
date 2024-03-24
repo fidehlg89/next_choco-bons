b087b19d6a1f0f6fd6c731ee64bee7825d8947aa
Fidel Ernesto <fidehlg89@gmail.com>
2024-03-23 20:03:33 -0400

    * feat(app): updated page content and styles * chore(ui): added global CSS variables * feat: added new components and utils * chore: updated package dependencies * chore: updated Tailwind config for dark mode and styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,11 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <strong>{`Luna's Choco Bons. Venta de Chocolates`}</strong>
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-teal-500-500 p-4 md:h-52">
+        <strong>{`Luna's Bokbonela. Haz tu encargo de Bombones`}</strong>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        {/* Prepare here contetn to be displayed */}
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
